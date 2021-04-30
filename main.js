9ecf2c0c68a86a21b80afded2da9f734d27fd162
Anish Kumar Barnwal <ak47barnwal@gmail.com>
2021-04-30 14:17:17 +0530

    Load multiple animations

--- main.js
+++ main.js
@@ -8,30 +8,48 @@ let loadImage = (src, callback) => {
   image.src = src;
 };
 
-let loadImages = (callback) => {
-  let images = [];
+let frames = {
+  idle: [1, 2, 3, 4, 5, 6, 7, 8],
+  kick: [1, 2, 3, 4, 5, 6, 7],
+  punch: [1, 2, 3, 4, 5, 6, 7],
+};
 
-  [1, 2, 3, 4, 5, 6, 7, 8].forEach((frameNumber) => {
-    let path = "images/" + frameNumber + ".png";
-    loadImage(path, (image) => {
-      images[frameNumber - 1] = image;
-      frameNumber === 8 && callback(images);
+let imagePath = (animation, frameNumber) => {
+  return `images/${animation}/${frameNumber}.png`;
+};
+
+let loadImages = (callback) => {
+  let images = { idle: [], kick: [], punch: [] };
+  let imagesToLoad = 0;
+
+  ["idle", "kick", "punch"].forEach((animation) => {
+    let animationFrames = frames[animation];
+    imagesToLoad += animationFrames.length;
+
+    animationFrames.forEach((frameNumber) => {
+      let path = imagePath(animation, frameNumber);
+
+      loadImage(path, (image) => {
+        images[animation][frameNumber - 1] = image;
+        imagesToLoad--;
+        imagesToLoad === 0 && callback(images);
+      });
     });
   });
 };
 
-let animate = (ctx, images, callback) => {
-  images.forEach((image, index) => {
+let animate = (ctx, images, animation, callback) => {
+  images[animation].forEach((image, index) => {
     setTimeout(() => {
       ctx.clearRect(0, 0, 500, 500);
       ctx.drawImage(image, 10, 10, 500, 500);
     }, index * 100);
   });
-  setTimeout(callback, images.length * 100);
+  setTimeout(callback, images[animation].length * 100);
 };
 
 loadImages((images) => {
-  animate(ctx, images, () => {
+  animate(ctx, images, "kick", () => {
     console.log("Done");
   });
 });
